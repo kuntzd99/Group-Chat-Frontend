4ef9ef55e7be4aee327327dfcea7f709c59bf2c5
Daniel Kuntz <kuntzd99@gmail.com>
2022-05-11 12:58:15 -0500

    Built out reactions in MessageCard.vue

--- groupchat/src/components/MessageCard.vue
+++ groupchat/src/components/MessageCard.vue
@@ -8,7 +8,7 @@
     <h6 class="username">{{ message.senderUsername }}</h6>
     <p class="message">{{ message.message }}</p>
     <div class="reactions">
-      &#128077;
+      <div class="reaction">{{ numLikes }} &#128077;</div>
     </div>
   </div>
 </template>
@@ -19,6 +19,34 @@ export default {
   props: {
     message: {},
     user: {}
+  },
+  data: () => ({
+    reactions: [],
+    numLikes: 0
+  }),
+  async mounted() {
+    await this.getReactions()
+  },
+  methods: {
+    async getReactions() {
+      const res = await axios.get('http://localhost:8000/reactions/')
+      for (let i = 0; i < res.data.length; i++) {
+        if (res.data[i].message === this.message.id) {
+          this.reactions.push(res.data[i])
+          if (res.data[i].type === 'like') {
+            this.numLikes++
+          }
+        }
+      }
+    },
+    async addReaction(type) {
+      await axios.post('http://localhost:8000/reactions/', {type: type, user: this.user.id, username: this.user.username, message: this.message.id})
+      await this.getReactions()
+    },
+    async removeReaction(reactionId) {
+      await axios.delete(`http://localhost:8000/reactions/${reactionId}`)
+      await this.getReactions()
+    }
   }
 }
 </script>
