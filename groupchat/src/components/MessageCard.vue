87fc040df734fbb6175ec7caf25fd6d371003544
Daniel Kuntz <kuntzd99@gmail.com>
2022-05-11 15:32:08 -0500

    Users can now remove their reactions

--- groupchat/src/components/MessageCard.vue
+++ groupchat/src/components/MessageCard.vue
@@ -6,11 +6,12 @@
       <div @click="() => addReaction('like')" class="reaction">{{ numLikes }} &#128077;</div>
     </div>
   </div>
-  <div v-else class="user-message">
+  <div v-else class="nonuser-message">
     <h6 class="username">{{ message.senderUsername }}</h6>
     <p class="message">{{ message.message }}</p>
     <div class="reactions">
-      <div @click="() => addReaction('like')" class="reaction">{{ numLikes }} &#128077;</div>
+      <div v-if="!liked" @click="() => addReaction('like')" class="reaction">{{ numLikes }} &#128077;</div>
+      <div v-else @click="() => removeReaction(likedReactionId)" class="reaction">{{ numLikes }} &#128077;</div>
     </div>
   </div>
 </template>
@@ -26,19 +27,26 @@ export default {
   },
   data: () => ({
     reactions: [],
-    numLikes: 0
+    numLikes: 0,
+    liked: false,
+    likedReactionId: -1
   }),
   async mounted() {
     await this.getReactions()
   },
   methods: {
     async getReactions() {
+      this.numLikes = 0
       const res = await axios.get('http://localhost:8000/reactions/')
       for (let i = 0; i < res.data.length; i++) {
         if (res.data[i].message === this.message.id) {
           this.reactions.push(res.data[i])
           if (res.data[i].type === 'like') {
             this.numLikes++
+            if (res.data[i].user === this.user.id) {
+              this.liked = true
+              this.likedReactionId = res.data[i].id
+            }
           }
         }
       }
@@ -49,6 +57,8 @@ export default {
     },
     async removeReaction(reactionId) {
       await axios.delete(`http://localhost:8000/reactions/${reactionId}`)
+      this.liked = false
+      this.likedReactionId = -1
       await this.getReactions()
     }
   }
